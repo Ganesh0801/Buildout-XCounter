11e9f73bb2e5cc30d2046fc9a982288f8eb9faf4
Ganesh <ganeshsbsa08@gmail.com>
2024-04-03 14:42:18 +0530

    second commit

--- src/Counter.js
+++ src/Counter.js
@@ -5,12 +5,20 @@ import style from "./Counter.module.css";
 const Counter = () => {
 
     const [count,setCount]=useState(0)
+
+  const Increment = ()=>{
+    setCount((prev)=>prev+1)
+  }
+
+  const Decrement = ()=>{
+    setCount((prev)=>prev - 1)
+  }
   return (
     <>
      <h1>Counter App</h1>
-     <h3>Count:{count}</h3>
-     <button onClick={()=>setCount(count+1)}>Increment</button>
-     <button onClick={()=>setCount(count-1)}>Decrement</button>
+     <p>Count: {count}</p>
+     <button onClick={Increment}>Increment</button>
+     <button onClick={Decrement}>Decrement</button>
     </>
   )
 }
